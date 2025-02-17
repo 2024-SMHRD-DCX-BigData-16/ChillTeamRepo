eeb4e9cb5c47519ab882bdfdb38f95aef5add5bb
NDS <thkjds7173@naver.com>
2025-02-17 16:22:40 +0900

    다시

--- TestTeam/src/Test/Ex01.java
+++ TestTeam/src/Test/Ex01.java
@@ -13,7 +13,7 @@ public class Ex01 {
 		System.out.println("헐렝방구");
 
 		System.out.println("어려워잉");
-		System.out.println("컨트롤 c 누르면 다시 전으로 돌아갑니당 꿀~팁~ㅋ");
+		
 		System.out.println("뇨내");
 		System.out.println("컨트롤 c 누르면 다시 무시하고 아래로 내려갑니동~");
 		System.out.println("pull 해라ㅡㅡ;");
